848c05448858f806d36b8fefccb0b3383d3da084
Rosie Tempest <rosietempest@gmail.com>
2021-01-03 09:27:45 -0800

    updated dates to years instead of days

--- chart.js
+++ chart.js
@@ -1,28 +1,29 @@
 const data_url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
 
-
-const fake_data = [66, 45, 34, 99, 104, 65, 44, 70, 75, 56, 72, 87]
-
 d3.json(data_url, function (json) {
     const data = json.data
 
-    w = 1200
+    w = 800
     h = 500
-    pX = 5
-    pY = 10
+    pX = 60
+    pY = 60
+
+    // create gdp and date lists
+    const data_date = data.map(item => item[0])
+
     // set the X axis scale
     const xScale = d3.scaleLinear()
-    const minX = d3.min(data, (d) => d[0])
-    const maxX = d3.max(data, (d) => d[0])
+    const minX = d3.min(data_date)
+    const maxX = d3.max(data_date)
 
     // calculate the difference in days between max and min
     var minDate = new Date(minX);
     var maxDate = new Date(maxX);
-    daysDiff = (maxDate.getTime() - minDate.getTime()) / (1000 * 3600 * 24)
-    console.log(daysDiff)
+    yearsDiff = (maxDate.getFullYear() - minDate.getFullYear())
+    console.log(yearsDiff)
 
     // set domain and range of scale to cover number of days
-    xScale.domain([0, daysDiff])
+    xScale.domain([0, yearsDiff])
         .range([pX, w - pX])
 
     // set the Y axis scale
@@ -30,7 +31,7 @@ d3.json(data_url, function (json) {
     const minY = d3.min(data, (d) => d[1])
     const maxY = d3.max(data, (d) => d[1])
     yScale.domain([0, maxY])
-        .range([0, h - pY])
+        .range([0, h])
 
 
     // create bar chart
@@ -44,14 +45,29 @@ d3.json(data_url, function (json) {
         .enter()
         .append('rect')
         .attr('class', 'bar')
-        .attr('width', 2)
+        .attr('width', 5)
         .attr('height', (d) => yScale(d[1]))
         .attr('x', (d, i) => {
-            let day = new Date(d[0]);
-            dayNum = (day.getTime() - minDate.getTime()) / (1000 * 3600 * 24)
-            console.log(xScale(dayNum))
-            return xScale(dayNum)
+            let year = new Date(d[0]);
+            yearNum = (year.getFullYear() - minDate.getFullYear())
+            console.log(xScale(yearNum))
+            return xScale(yearNum)
         })
-        .attr('y', (d, i) => h - yScale(d[1]))
+        .attr('y', (d, i) => h - yScale(d[1]) - pY)
+
+    // add the axes
+    const xAxis = d3.axisBottom().scale(xScale)
+
+    svg.append("g")
+        .attr("id", 'x-axis')
+        .attr("transform", "translate(0," + (h - pY / 2) + ")")
+        .call(xAxis);
+
+    const yAxis = d3.axisRight(yScale)
+    svg.append("g")
+        .attr("id", 'y-axis')
+        .attr("transform", "translate(60,0)")
+        .call(yAxis);
 
 });
+
