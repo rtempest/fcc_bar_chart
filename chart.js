c8620ebf586332e46afd3ef1b90c058dacca48af
Rosie Tempest <rosietempest@gmail.com>
2021-01-05 08:51:07 -0800

    fixed the data-date attribute

--- chart.js
+++ chart.js
@@ -2,60 +2,61 @@ const data_url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenc
 
 d3.json(data_url, function (json) {
     const data = json.data
+    // console.log(data)
 
-    w = 800
+    w = 950
     h = 500
     p = 40
 
     // create gdp and date lists
-    const data_date = data.map(item => item[0])
+    let formatDate = d3.timeFormat("%Y-%m-%d")
+    const data_date = data.map(item => new Date(item[0] + "T00:00"))
+    // console.log(typeof data_date[3])
+    // console.log(data_date[0])
+
     const gdp = data.map(item => item[1])
 
     // set the X axis scale
-    const xScale = d3.scaleLinear()
+    // console.log(data_date)
     const minX = d3.min(data_date)
     const maxX = d3.max(data_date)
-
-    // calculate the difference in years between max and min
-    var minDate = new Date(minX);
-    var maxDate = new Date(maxX);
-    const firstYear = minDate.getFullYear()
-    const lastYear = maxDate.getFullYear()
-    // yearsDiff = (maxDate.getFullYear() - minDate.getFullYear())
-    console.log(firstYear)
-
-    // set domain and range of scale to cover number of days
-    xScale.domain([firstYear, lastYear])
+    console.log(minX)
+    console.log(maxX)
+    // set domain and range of scale
+    const xScale = d3.scaleTime()
+        .domain([minX, maxX])
         .range([p, w - p])
 
     // set the Y axis scale
     const yScale = d3.scaleLinear()
     const minY = d3.min(gdp)
-    console.log(minY)
     const maxY = d3.max(gdp)
     yScale.domain([0, maxY])
         .range([h - p, p])
 
+    const container = d3.select('#chart')
+        .append('text', 'hello')
+        .append('svg')
 
     // create bar chart
     const svg = d3.select("#chart")
         .append("svg")
         .attr("width", w)
-        .attr("height", h);
+        .attr("height", h)
 
     const bars = svg.selectAll('rect')
         .data(data)
         .enter()
         .append('rect')
-        .attr('data-date', (d, i) => data_date[i])
+        .attr('data-date', (d, i) => d[0])
         .attr('data-gdp', (d, i) => gdp[i])
         .attr('class', 'bar')
-        .attr('width', 5)
+        .attr('width', 2)
         .attr('height', (d) => h - yScale(d[1]) - p)
         .attr('x', (d, i) => {
-            let year = new Date(d[0]);
-            yearNum = (year.getFullYear())
-            return xScale(yearNum)
+            let year = new Date(data_date[i]);
+            console.log(year)
+            return xScale(year)
         })
         .attr('y', (d, i) => yScale(d[1]))
 
@@ -83,12 +84,19 @@ d3.json(data_url, function (json) {
 
     // add the tooltip on mouseover event
     bars
-        .on('mouseover', () => d3.select('#tooltip').style("opacity", 1))
-        .on("mousemove", function () {
-            return tooltip.style("top", (d3.event.pageY - 10) + "p")
-                .style("left", (d3.event.pageX + 10) + "p");
+        .on('mouseover', () => {
+            d3.select('#tooltip').style("opacity", 1)
+                .style("left", d3.event.pageX)
+                .style("background-color", 'blue')
+                .style("top", d3.event.pageY)
+                .style("display", "inline-block")
+
         })
-        .on('mouseout', () => d3.select('#tooltip').style("opacity", 0))
+    // .on("mousemove", function (event) {
+    //     return tooltip.style("top", (d3.event.pageY - 10) + "p")
+    //         .style("left", (d3.event.pageX + 10) + "p");
+    // })
+    // .on('mouseout', () => d3.select('#tooltip').style("opacity", 0))
 });
 
 
